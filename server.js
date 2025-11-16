bda738b8b2145addc4d3ae3ade42cbd148a37338
Blackwizard <142197906+Balackwizard@users.noreply.github.com>
2025-11-16 23:12:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = '5376895931:AAGungqejutol8QIidI2tlJMkmCSUuAAY9w'
-const id = '5155109916'
+const token = '8267689220:AAGFVJkGBcYnjU-KbH6pkGqUan6kdpkf_k8'
+const id = '8348574306'
 const address = 'https://www.google.com'
 
 const app = express();
