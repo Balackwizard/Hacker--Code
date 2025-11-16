e049a37fc3cbbdb541c27bca8172f38ce5058805
Blackwizard <142197906+Balackwizard@users.noreply.github.com>
2025-11-16 06:56:41 -0800

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = process.env.bot_token
-const id = process.env.bot_id
+const token ='https://hacker-code-8mqq.onrender.com'
+const id = '8348574306'
 const address = 'https://www.google.com'
 
 const app = express();
